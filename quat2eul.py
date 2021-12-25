c34d49ed74327d274adec0970549201cc406e65e
ppecheux <pierrelouis.pecheux@gmail.com>
2021-12-25 19:27:54 +0100

    add sequence enum

--- quat2eul.py
+++ quat2eul.py
@@ -1,88 +1,121 @@
+from typing import Callable, Tuple
 import numpy as np
 from math import pi
+from enum import Enum
 
-def quat2eul(sequence, q):
+
+class Sequence(Enum):
+    XYX = "xyx"
+    YZY = "yzy"
+    ZXZ = "zxz"
+    XZX = "xzx"
+    YXY = "yxy"
+    ZYZ = "zyz"
+    XYZ = "xyz"
+    YZX = "yzx"
+    ZXY = "zxy"
+    XZY = "xzy"
+    YXZ = "yxz"
+    ZYX = "zyx"
+
+    @property
+    def singularity_group(self) -> int:
+        len(set(self.value())) - 1
+
+
+def quat2eul(sequence: Sequence, q: Callable) -> Tuple[float]:
     """
     Renvoie les angles d'euler pour les quaternions en entrée (liste q des quaternions) suivant la sequence de rotation (string sequence des trois axes de rotation)
-    exemple : quat2eul("xyx", [0.0, 0.1, 0.0, 0.0])
+    exemple : quat2eul(Sequence.XYX , [0.0, 0.1, 0.0, 0.0])
     """
-    psi = 0
-    theta = 0
-    phi = 0
+    psi = theta = phi = 0
 
-    if ("xyx" == sequence):
-        psi = np.arctan2((q[1] * q[2] + q[3] * q[0]), (q[2] * q[0] - q[1] * q[3]))
-        theta = np.arccos(q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3])
-        phi = np.arctan2((q[1] * q[2] - q[3] * q[0]), (q[1] * q[3] + q[2] * q[0]))
-        singularitycheck(1, theta)
-    elif ("yzy" == sequence) :
-        psi = np.arctan2((q[1] * q[0] + q[2] * q[3]), (q[3] * q[0] - q[1] * q[2]))
-        theta = np.arccos(q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3])
-        phi = np.arctan2((q[2] * q[3] - q[1] * q[0]), (q[1] * q[2] + q[3] * q[0]))
-        singularitycheck(1, theta)
-    elif ("zxz" == sequence) :
-        psi = np.arctan2((q[1] * q[3] + q[2] * q[0]), (q[1] * q[0] - q[2] * q[3]))
-        theta = np.arccos(q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3])
-        phi = np.arctan2((q[1] * q[3] - q[2] * q[0]), (q[1] * q[0] + q[2] * q[3]))
-        singularitycheck(1, theta)
-    elif ("xzx" == sequence) :
-        psi = np.arctan2((q[1] * q[3] - q[2] * q[0]), (q[1] * q[2] + q[3] * q[0]))
-        theta = np.arccos(q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3])
-        phi = np.arctan2((q[1] * q[3] + q[2] * q[0]), (q[3] * q[0] - q[1] * q[2]))
-        singularitycheck(1, theta)
-    elif ("yxy" == sequence) :
-        psi = np.arctan2((q[1] * q[2] - q[3] * q[0]), (q[1] * q[0] + q[2] * q[3]))
-        theta = np.arccos(q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3])
-        phi = np.arctan2((q[1] * q[2] + q[3] * q[0]), (q[1] * q[0] - q[2] * q[3]))
-        singularitycheck(1, theta)
-    elif ("zyz" == sequence) :
-        psi = np.arctan2((q[2] * q[3] - q[1] * q[0]), (q[1] * q[3] + q[2] * q[0]))
-        theta = np.arccos(q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3])
-        phi = np.arctan2((q[1] * q[0] + q[2] * q[3]), (q[2] * q[0] - q[1] * q[3]))
-        singularitycheck(1, theta)
-    elif ("xyz" == sequence) :
-        psi = np.arctan2(2 * (q[1] * q[0] - q[2] * q[3]), (q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]))
+    if Sequence.XYX == sequence:
+        psi = np.arctan2((q[1] * q[2] + q[3] * q[0]),
+                         (q[2] * q[0] - q[1] * q[3]))
+        theta = np.arccos(q[0] * q[0] + q[1] * q[1] -
+                          q[2] * q[2] - q[3] * q[3])
+        phi = np.arctan2((q[1] * q[2] - q[3] * q[0]),
+                         (q[1] * q[3] + q[2] * q[0]))
+    elif Sequence.YZY == sequence:
+        psi = np.arctan2((q[1] * q[0] + q[2] * q[3]),
+                         (q[3] * q[0] - q[1] * q[2]))
+        theta = np.arccos(q[0] * q[0] - q[1] * q[1] +
+                          q[2] * q[2] - q[3] * q[3])
+        phi = np.arctan2((q[2] * q[3] - q[1] * q[0]),
+                         (q[1] * q[2] + q[3] * q[0]))
+    elif Sequence.ZXZ == sequence:
+        psi = np.arctan2((q[1] * q[3] + q[2] * q[0]),
+                         (q[1] * q[0] - q[2] * q[3]))
+        theta = np.arccos(q[0] * q[0] - q[1] * q[1] -
+                          q[2] * q[2] + q[3] * q[3])
+        phi = np.arctan2((q[1] * q[3] - q[2] * q[0]),
+                         (q[1] * q[0] + q[2] * q[3]))
+    elif Sequence.XZX == sequence:
+        psi = np.arctan2((q[1] * q[3] - q[2] * q[0]),
+                         (q[1] * q[2] + q[3] * q[0]))
+        theta = np.arccos(q[0] * q[0] + q[1] * q[1] -
+                          q[2] * q[2] - q[3] * q[3])
+        phi = np.arctan2((q[1] * q[3] + q[2] * q[0]),
+                         (q[3] * q[0] - q[1] * q[2]))
+    elif Sequence.YXY == sequence:
+        psi = np.arctan2((q[1] * q[2] - q[3] * q[0]),
+                         (q[1] * q[0] + q[2] * q[3]))
+        theta = np.arccos(q[0] * q[0] - q[1] * q[1] +
+                          q[2] * q[2] - q[3] * q[3])
+        phi = np.arctan2((q[1] * q[2] + q[3] * q[0]),
+                         (q[1] * q[0] - q[2] * q[3]))
+    elif Sequence.ZYZ == sequence:
+        psi = np.arctan2((q[2] * q[3] - q[1] * q[0]),
+                         (q[1] * q[3] + q[2] * q[0]))
+        theta = np.arccos(q[0] * q[0] - q[1] * q[1] -
+                          q[2] * q[2] + q[3] * q[3])
+        phi = np.arctan2((q[1] * q[0] + q[2] * q[3]),
+                         (q[2] * q[0] - q[1] * q[3]))
+    elif Sequence.XYZ == sequence:
+        psi = np.arctan2(2 * (q[1] * q[0] - q[2] * q[3]),
+                         (q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]))
         theta = np.arcsin(2 * (q[1] * q[3] + q[2] * q[0]))
-        phi = np.arctan2(2 * (q[3] * q[0] - q[1] * q[2]), (q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]))
-        singularitycheck(2, theta)
-    elif ("yzx" == sequence) :
-        psi = np.arctan2(2 * (q[2] * q[0] - q[1] * q[3]), (q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]))
+        phi = np.arctan2(2 * (q[3] * q[0] - q[1] * q[2]),
+                         (q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]))
+    elif Sequence.YZX == sequence:
+        psi = np.arctan2(2 * (q[2] * q[0] - q[1] * q[3]),
+                         (q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]))
         theta = np.arcsin(2 * (q[1] * q[2] + q[3] * q[0]))
-        phi = np.arctan2(2 * (q[1] * q[0] - q[3] * q[2]), (q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3]))
-        singularitycheck(2, theta)
-    elif ("zxy" == sequence) :
-        psi = np.arctan2(2 * (q[3] * q[0] - q[1] * q[2]), (q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3]))
+        phi = np.arctan2(2 * (q[1] * q[0] - q[3] * q[2]),
+                         (q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3]))
+    elif Sequence.ZXY == sequence:
+        psi = np.arctan2(2 * (q[3] * q[0] - q[1] * q[2]),
+                         (q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3]))
         theta = np.arcsin(2 * (q[1] * q[0] + q[2] * q[3]))
-        phi = np.arctan2(2 * (q[2] * q[0] - q[3] * q[1]), (q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]))
-        singularitycheck(2, theta)
-    elif ("xzy" == sequence) :
-        psi = np.arctan2(2 * (q[1] * q[0] + q[2] * q[3]), (q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3]))
+        phi = np.arctan2(2 * (q[2] * q[0] - q[3] * q[1]),
+                         (q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]))
+    elif Sequence.XZY == sequence:
+        psi = np.arctan2(2 * (q[1] * q[0] + q[2] * q[3]),
+                         (q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3]))
         theta = np.arcsin(2 * (q[3] * q[0] - q[1] * q[2]))
-        phi = np.arctan2(2 * (q[1] * q[3] + q[2] * q[0]), (q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]))
-        singularitycheck(2, theta)
-    elif ("yxz" == sequence) :
-        psi = np.arctan2(2 * (q[1] * q[3] + q[2] * q[0]), (q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]))
+        phi = np.arctan2(2 * (q[1] * q[3] + q[2] * q[0]),
+                         (q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]))
+    elif Sequence.YXZ == sequence:
+        psi = np.arctan2(2 * (q[1] * q[3] + q[2] * q[0]),
+                         (q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]))
         theta = np.arcsin(2 * (q[1] * q[0] - q[2] * q[3]))
-        phi = np.arctan2(2 * (q[1] * q[2] + q[3] * q[0]), (q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3]))
-        singularitycheck(2, theta)
-    elif ("zyx" == sequence) :
-        psi = np.arctan2(2 * (q[1] * q[2] + q[3] * q[0]), (q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]))
+        phi = np.arctan2(2 * (q[1] * q[2] + q[3] * q[0]),
+                         (q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3]))
+    else:
+        psi = np.arctan2(2 * (q[1] * q[2] + q[3] * q[0]),
+                         (q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]))
         theta = np.arcsin(2 * (q[2] * q[0] - q[1] * q[3]))
-        phi = np.arctan2(2 * (q[1] * q[0] + q[3] * q[2]), (q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]))
-        singularitycheck(2, theta)
-    # // matlab way:
-    # // psi = np.arctan2( 2.*(q[1]*q[2] + q[0]*q[3]) , q[0]*q[0] + q[1]*q[1]- q[2]*q[2] - q[3]*q[3])
-    # // theta = np.arcsin( 2.*( - q[1]*q[3] + q[0]*q[2]))
-    # // phi = np.arctan2( 2.*(q[2]*q[3] + q[0]*q[1]) , q[0]*q[0] - q[1]*q[1]- q[2]*q[2] + q[3]*q[3])
-    else :
-        print("The sequence is not in the expected format (\"xyz\")\n")
+        phi = np.arctan2(2 * (q[1] * q[0] + q[3] * q[2]),
+                         (q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]))
 
-    return(psi,theta,phi)
+    if sequence.singularity_group == 1 and pi-theta < pi/180:
+        raise SingularityError(f'{pi-theta} || {theta} < rad')
+    elif sequence.singularity_group == 2 and abs(theta-pi/2) < pi/180:
+        raise SingularityError(f'{abs(theta-pi/2)} < {pi/180} rad')
 
-def singularitycheck(group, theta):
-    if (group==1 and pi-theta<pi/180):
-        print(f"singularity check failed: {pi-theta} || {theta} < rad\n")
-    elif (group==2 and abs(theta-pi/2)<pi/180):
-        print(f"singularity check failed: {abs(theta-pi/2)} < {pi/180} rad\n")
-    elif (group != 1 and group != 2):
-        print("the group must be 1 or 2")
+    return psi, theta, phi
+
+
+class SingularityError(Exception):
+    pass
